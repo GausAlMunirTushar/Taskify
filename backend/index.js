c158cc836ea4b8708990396b6b791f82d24cbfaf
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-02-20 13:15:08 +0600

    package.json file added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
-const port = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(port, ()=>{
-    console.log(`Server is Running on http://localhost:${port}`)
+app.listen(PORT, ()=>{
+    console.log(`Server is Running on ${PORT}`)
 })
