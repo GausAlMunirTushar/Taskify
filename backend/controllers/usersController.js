b9cfe07c1536f91b39d9efd504f015fda0fe64a5
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-02-23 23:37:15 +0600

    user controller status 200 update

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -6,7 +6,7 @@ const registration = (req, res)=>{
     let reqBody = req.body;
     usersModel.create(reqBody, (err, data)=>{
         if(err){
-            res.status(400).json({status:"failed", data: err})
+            res.status(200).json({status:"failed", data: err})
         }
         else{
             res.status(200).json({status:"success", data: data})
