370a528dfc58ba37858c5b5624245349e4d2a384
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-12 22:59:00 +0600

    build added on master

---
+++ frontend/build/static/js/458.38666bbd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[458],{9458:function(n,e,t){t.r(e);t(2791);var u=t(184);e.default=function(){return(0,u.jsx)("div",{})}}}]);
+//# sourceMappingURL=458.38666bbd.chunk.js.map
---
+++ frontend/build/static/js/738.44cf2866.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[738],{7738:function(n,e,t){t.r(e);t(2791);var u=t(184);e.default=function(){return(0,u.jsx)("div",{})}}}]);
+//# sourceMappingURL=738.44cf2866.chunk.js.map
