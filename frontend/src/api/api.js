1a692ece5e64fdbf6cdb87a4e0699ed1b4378f5f
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2023-10-13 04:21:14 +0600

    login completed and session set

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,71 +1,71 @@
-import axios from 'axios'
-import { errorToast, successToast } from '../helper/FormValid';
-import store from '../redux/store/store'
-import {showLoader, hideLoader} from '../redux/state/settingSlice'
-import {setToken, setUserDetails} from '../helper/Session'
-const baseURL = 'https://taskmasterx-backend.onrender.com/api/v1';
+    import axios from 'axios'
+    import { errorToast, successToast } from '../helper/FormValid';
+    import store from '../redux/store/store'
+    import {showLoader, hideLoader} from '../redux/state/settingSlice'
+    import {setToken, setUserDetails} from '../helper/Session'
+    const baseURL = 'https://task-backend-pvop.onrender.com/api/v1';
 
-const registrationRequest = async (email, firstName, lastName, mobileNumber, password, photo)=>{
-    store.dispatch(showLoader())
-    let URL = baseURL +"/registration";
-    let postBody = {
-        email: email,
-        firstName: firstName,
-        lastName: lastName,
-        mobileNumber: mobileNumber,
-        password: password,
-        photo: photo
-    }
-    return axios.post(URL, postBody).then((res)=>{
-        store.dispatch(hideLoader())
-        if(res.status === 200){
-            if(res.data['status'] === "fail"){
-                if(res.data['data']['keyPattern']['email']===1){
-                    errorToast("Email Already Exist")
-                    return false;
+    const registrationRequest = async (email, firstName, lastName, mobileNumber, password, photo)=>{
+        store.dispatch(showLoader())
+        let URL = baseURL +"/registration";
+        let postBody = {
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            mobileNumber: mobileNumber,
+            password: password,
+            photo: photo
+        }
+        return axios.post(URL, postBody).then((res)=>{
+            store.dispatch(hideLoader())
+            if(res.status === 200){
+                if(res.data['status'] === "fail"){
+                    if(res.data['data']['keyPattern']['email']===1){
+                        errorToast("Email Already Exist")
+                        return false;
+                    }
+                    else{
+                        errorToast("Something Went Wrong")
+                        return false;
+                    }
                 }
-                else{
-                    errorToast("Something Went Wrong")
-                    return false;
+                else {
+                    successToast("Registration Success")
+                    return true;
                 }
             }
-            else {
-                successToast("Registration Success")
+            else{
+                errorToast("Something Went Wrong")
+                return  false;
+            }
+        }).catch((err)=>{
+            store.dispatch(hideLoader())
+            errorToast("Something Went Wrong")
+            return false;
+        })
+    }
+    export const loginRequest = async (email, password) =>{
+        store.dispatch(showLoader())
+        let URL = baseURL +"/login";
+        let postBody = {"email": email,"password": password}
+        return axios.post(URL, postBody).then((res)=>{
+            store.dispatch(hideLoader());
+            if(res.status===200){
+                setToken(res.data['token']);
+                setUserDetails(res.data['data']);
+                successToast("Login Success")
                 return true;
             }
-        }
-        else{
-            errorToast("Something Went Wrong")
-            return  false;
-        }
-    }).catch((err)=>{
-        store.dispatch(hideLoader())
-        errorToast("Something Went Wrong")
-        return false;
-    })
-}
-export const loginRequest = async (email, password)=>{
-    store.dispatch(showLoader())
-    let URL = baseURL +"/login";
-    let postBody = {"email": email,"passoword": password}
-    return axios.post(URL, postBody).then((res)=>{
-        store.dispatch(hideLoader());
-        if(res.status===200){
-            setToken(res.data['token']);
-            setUserDetails(res.data['data']);
-            successToast("Login Success")
-            return true;
-        }
-        else{
-            errorToast("Invalid Email or Password")
-            return  false;
-        }
+            else{
+                errorToast("Invalid Email or Password")
+                return  false;
+            }
 
-    }).catch((err)=>{
-        console.log(err)
-        errorToast("Something Went Wrong")
-        store.dispatch(hideLoader())
-        return false;
-    })
-}
-export default registrationRequest;
+        }).catch((err)=>{
+            console.log(err)
+            errorToast("Something Went Wrong")
+            store.dispatch(hideLoader())
+            return false;
+        })
+    }
+    export default registrationRequest;
--- frontend/src/helper/Session.js
+++ frontend/src/helper/Session.js
@@ -1,22 +1,26 @@
 class Session {
-    setToken(token){
-        localStorage.setItem("token",token)
-    }
-    getToken(){
-        return localStorage.getItem("token")
-    }
-    setUserDetails(userDetails){
-        localStorage.setItem("userDetails", JSON.stringify(userDetails))
-    }
-    getUserDetails(){
-        return JSON.parse(localStorage.getItem("userDetails"))
+    setToken(token) {
+        localStorage.setItem("token", token);
     }
 
+    getToken() {
+        return localStorage.getItem("token");
+    }
+
+    setUserDetails(userDetails) {
+        localStorage.setItem("userDetails", JSON.stringify(userDetails));
+    }
+
+    getUserDetails() {
+        return JSON.parse(localStorage.getItem("userDetails"));
+    }
 }
+
+const session = new Session(); // Create an instance of the Session class
 
 export const {
     setToken,
     getToken,
     setUserDetails,
     getUserDetails
-} = new Session();
+} = session;
